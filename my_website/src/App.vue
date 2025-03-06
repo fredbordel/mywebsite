8ffc9423e6b0fb5f790154a03c08f0e571cd2448
Fred Bordel <frederique.bordeleau@gmail.com>
2025-03-05 21:47:04 -0500

    Initialize Vue project + creation of json dictionary

---
+++ my_website/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>{{ dict[selectedLang].app.h1 }}</h1>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">{{ dict[selectedLang].home.title }}</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { useLang } from "./stores/lang";
+import dict from "./assets/dictionary.json";
+
+const { selectedLang } = useLang();
+</script>
+
+<style></style>
---
+++ my_website/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>Maison View</h2>
+</template>
+
+<script setup></script>
